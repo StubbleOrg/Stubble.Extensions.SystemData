337cbc2637f76b975d1a562d5b758bb80134c45c
Romanx <mcauliffe.alex@gmail.com>
2015-07-28 13:20:34 +0100

    Added truthy check for DataTables

---
+++ Stubble.Extensions.SystemData/SystemData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stubble.Core.Interfaces;
+
+namespace Stubble.Extensions.SystemData
+{
+    public static class SystemData
+    {
+        public static IStubbleBuilder AddSystemData(this IStubbleBuilder builder)
+        {
+            builder.AddTruthyCheck(DataTableTruthyCheck);
+            return builder;
+        }
+
+        private static bool? DataTableTruthyCheck(object obj)
+        {
+            var table = obj as DataTable;
+            if (table != null)
+            {
+                return table.Rows.Count > 0;
+            }
+            return null;
+        }
+    }
+}
