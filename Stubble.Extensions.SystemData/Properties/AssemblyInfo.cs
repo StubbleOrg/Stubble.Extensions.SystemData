024ae462a6063896ea85acc63f6c8c5f2e1ef833
Romanx <mcauliffe.alex@gmail.com>
2015-07-28 12:57:32 +0100

    Initial commit

---
+++ Stubble.Extensions.SystemData/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Stubble.Extensions.SystemData")]
+[assembly: AssemblyDescription("Extensions Adding Value Getters for System.Data Types")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alex McAuliffe")]
+[assembly: AssemblyProduct("Stubble.Extensions.SystemData")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: InternalsVisibleTo("Stubble.Extensions.SystemData.Tests")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e2735979-1154-4011-b2b5-8b82ae311e62")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Stubble Extensions - System.Data
