a180ef5b02d688df1687a89be80f0a018ad5632f
Romanx <mcauliffe.alex@gmail.com>
2015-07-28 15:38:43 +0100

    Update to Alpha 6 add test for dot enumeration

--- Stubble.Extensions.SystemData.Tests/SystemDataTest.cs
+++ Stubble.Extensions.SystemData.Tests/SystemDataTest.cs
@@ -99,5 +99,29 @@ namespace Stubble.Extensions.SystemData.Tests
             var output = stubble.Render("{{#foo.TableA}}{{IntColumn}}{{/foo.TableA}}-{{#foo.TableB}}{{IntColumn}}{{/foo.TableB}}", new { foo = ds });
             Assert.Equal("123-321", output);
         }
+
+        [Fact]
+        public void It_Should_Enumerate_Through_DataSet()
+        {
+            var dt = new DataTable("TableA");
+            dt.Columns.Add("IntColumn", typeof(int));
+            dt.Rows.Add(1);
+            dt.Rows.Add(2);
+            dt.Rows.Add(3);
+
+            var dt2 = new DataTable("TableB");
+            dt2.Columns.Add("IntColumn", typeof(int));
+            dt2.Rows.Add(3);
+            dt2.Rows.Add(2);
+            dt2.Rows.Add(1);
+
+            var ds = new DataSet();
+            ds.Tables.Add(dt);
+            ds.Tables.Add(dt2);
+            var stubble = new StubbleBuilder().AddSystemData().Build();
+
+            var output = stubble.Render("{{#foo}}{{#.}}{{IntColumn}}{{/.}}{{/foo}}", new { foo = ds });
+            Assert.Equal("123321", output);
+        }
     }
 }
